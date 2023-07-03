8d00e7a22d527863f6d69aa492dbefec54db84e4
sai kumar navara <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2023-07-03 16:37:55 +0000

    final commit

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoItems from './TodoItems'
 const AddTodo = () => {
     const [item, setItem] = useState([])
@@ -9,23 +9,29 @@ const AddTodo = () => {
     const TodoAddItem = () => {
         setItem([...item, value])
         setValue('')
+        localStorage.setItem('todo', JSON.stringify([...item, value]))
     }
     const deleteTodo = (id) => {
         const filtered = item.filter((item) => item !== id)
         setItem(filtered)
     }
-    const EditTodo = (item) => {
-        setValue(item)
-    }
+
+    const storedData = localStorage.getItem('todo');
+    const data = JSON.parse(storedData)
+    useEffect(() => {
+        if (data) {
+            setItem(data)
+        }
+    }, [])
     return (
         <div className='container'>
             <h1 className='text-center text-success'>Todo List</h1>
             <div className='d-flex justify-content-center'>
-                <input type='text' placeholder='Add Todo' value={value} onChange={TodolistItem} />
+                <input type='text' placeholder='Add Todo' className='mr-2' value={value} onChange={TodolistItem} />
                 <button className='btn btn-primary' onClick={TodoAddItem}>ADD</button>
             </div>
             <div className='d-flex justify-content-center'>
-                <TodoItems todo={item} deleteItem={deleteTodo} editTodo={EditTodo} />
+                <TodoItems todo={item} deleteItem={deleteTodo} />
             </div>
 
         </div>
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react'
-
-const TodoItems = ({ todo, deleteItem, editTodo }) => {
+import { AiFillDelete } from "react-icons/ai";
+const TodoItems = ({ todo, deleteItem }) => {
     const todoList = todo
     const [Todo, setTodo] = useState()
     useEffect(() => {
         setTodo(todo)
     }, [todo])
 
-
-    if (Todo === undefined) {
+    console.log(Todo, 'gabbar')
+    if (Todo === undefined || Todo.length === 0) {
         return (
             <div><h1>welcome</h1>  </div>
 
         )
     }
     return (
-        <div className='container'>
+        <div className='container-sm pt-5' style={{ maxWidth: '400px' }}>
             <div className='col'>
                 {Todo.map((item, index) => (
-
-                    <div className='card shadow' key={index}>
-                        <div className='card-body'>
-                            {item}
-                        </div>
-                        <div>
-                            <button onClick={() => { deleteItem(item) }}>delete</button>
-                            <button onClick={() => { editTodo(item) }}>Edit</button>
+                    <div className='card shadow mb-2 bg-primary text-white' key={index} >
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <div className='card-body'>
+                                {item}
+                            </div>
+                            <div>
+                                <h3 onClick={() => { deleteItem(item) }}>{<AiFillDelete />}</h3>
+                            </div>
                         </div>
                     </div>
                 ))}
